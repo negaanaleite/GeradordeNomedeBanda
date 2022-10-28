f213c20a4305b56327a49437e6d2862ca571c10a
Ana Leite <>
2022-10-28 14:52:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("------------------------------------------")
+print("  Bem vindo ao Gerador de nomes de banda!")
+print("------------------------------------------")
+cidade = input("Qual o nome da cidade que você nasceu \n")
+pet = input("Qual o nome do seu animel de estimação? \n")
+banda = pet +" "+ cidade
+print(banda)
